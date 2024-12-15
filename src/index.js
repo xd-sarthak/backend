706a65304599eab89a94ec0dccbf85f5e676b03b
xd-sarthak <sarthak.sri02032gmail.com>
2024-12-15 22:01:34 +0530

    mongo connected

---
+++ src/index.js
@@ -0,0 +1,60 @@
+
+import connectDB from './database/index.js';
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//METHOD 1:
+// const app = express();
+
+// (async () => {
+//     try {
+//       // Connect to MongoDB
+//       const dbUri = `${process.env.MONGODB_URI}/${process.env.DB_NAME}`;
+//       await mongoose.connect(dbUri, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       });
+  
+//       console.log("Database connected successfully");
+  
+//       // Handle application errors
+//       app.on("error", (error) => {
+//         console.error("Application Error:", error);
+//       });
+  
+//       // Start the server
+//       const port = process.env.PORT || 3000;
+//       app.listen(port, () => {
+//         console.log(`App is listening on port ${port}`);
+//       });
+//     } catch (error) {
+//       console.error("Startup Error:", error);
+//       process.exit(1); // Exit the process with failure
+//     }
+//   })();
+  
