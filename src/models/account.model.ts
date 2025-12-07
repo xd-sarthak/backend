d67f326242fb51357d467c6862d7e31563dc904b
xd-sarthak <sarthak.sri02032gmail.com>
2025-12-07 19:22:35 +0530

    stuff fixed

--- src/models/account.model.ts
+++ src/models/account.model.ts
@@ -5,7 +5,7 @@ export interface AccountDocument extends Document {
   provider: ProviderEnumType;
   providerId: string; // Store the email, googleId, facebookId as the providerId
   userId: mongoose.Types.ObjectId;
-  refreshToken: string | null;
+  refreshToken?: string | null;
   tokenExpiry: Date | null;
   createdAt: Date;
 }
